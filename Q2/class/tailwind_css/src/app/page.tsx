a3fdc159bf57c8430eb03d62389f3e76ba2fb91e
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-11-30 23:45:43 +0500

    card style

--- Q2/class/tailwind_css/src/app/page.tsx
+++ Q2/class/tailwind_css/src/app/page.tsx
@@ -1,10 +1,25 @@
 
+import Card1 from '@/components/amazon/Card1'
+import Card2 from '@/components/amazon/Card2'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
-  
+  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3'>
+<Card1/>
+ <Card2/>
+ <Card2/>
+<Card1/>
+<Card1/>
+ <Card2/>
+ <Card2/>
+<Card1/>
+
+
+
+  </div>
     </>
   )
 }
--- Q2/class/tailwind_css/src/components/amazon/Card2.tsx
+++ Q2/class/tailwind_css/src/components/amazon/Card2.tsx
@@ -1,9 +1,36 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Card2 = () => {
   return (
-    <div>Card2</div>
+    <div className='p-4 bg-white'>
+      <h2 className='text-xl font-bold mb-2'>Toys under $25</h2>
+      <div className='grid grid-cols-2 gap-2'>
+       
+<MiniCard />
+<MiniCard />
+<MiniCard />
+<MiniCard />
+      </div>
+      <p className='mt-5 text-[12px] text-[rgb(0,113,133)]'>Shop now</p>
+    </div>
   )
 }
 
 export default Card2
+
+
+export function MiniCard(){
+  return (
+    <div>
+    <Image 
+  className='w-full'
+  width={200}
+  height={200}
+  alt=''
+  src={`https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v3._SY304_CB573698005_.jpg`}
+  />
+  <span className='text-[12px] -mt-1'>Dining</span>
+    </div>
+  )
+}
